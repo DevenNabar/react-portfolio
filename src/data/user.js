81d0813189c32ee6eb285088771096e9782f5d20
Deven Nabar <68647977+DevenNabar@users.noreply.github.com>
2024-06-09 18:07:47 -0700

    Initial Commit

---
+++ src/data/user.js
@@ -0,0 +1,80 @@
+const INFO = {
+	main: {
+		title: "Deven's Portfolio",
+		name: "Deven Nabar",
+		email: "dnabar@usc.edu",
+		logo: "../logo.png",
+	},
+
+	socials: {
+		github: "https://github.com/devennabar",
+		linkedin: "https://linkedin.com/in/devennabar",
+	},
+
+	homepage: {
+		title: "Hi 👋, I'm Deven Nabar, Software Developer 👨‍💻, Based in Los Angeles 🏝️, California  ☀️",
+		description:
+			"I am a skilled software engineer leveraging my expertise in web technologies, machine learning, and databases to develop innovative solutions across various domains. I am advancing my knowledge through a Master's degree in Computer Science at the University of Southern California, focusing on learning innovative topics and techniques. With valuable experience from internships at Hub Matrix Technologies, VB Consultancy, and Techstride Global, I have honed my software development, web development, testing, and debugging skills. I am dedicated to creating efficient, reliable, and user-friendly software, thriving in diverse and collaborative team environments. Driven by a passion to contribute to advancing technology and society, I aim to leverage my technical expertise and educational background towards impactful projects. I enjoy running and frequenting the gym in my leisure time, maintaining a balanced lifestyle.",
+	},
+
+	about: {
+		title: "About Me",
+		description:
+			"Currently pursuing a Master of Science in Computer Science at the University of Southern California with a focus on Analysis of Algorithms, Web Technologies, Machine Learning, Natural Language Processing, Information Retrieval and Database Systems. With hands-on experience in web and software development, I am passionate about leveraging innovative technologies to solve real-world challenges.",
+	},
+
+	articles: {
+		title: "I'm passionate about pushing the boundaries of what's possible and inspiring the next generation of innovators.",
+		description:
+			"Chronological collection of my long-form thoughts on programming, leadership, product design, and more.",
+	},
+
+	projects: [
+		{
+			title: "Project 1",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+
+		{
+			title: "Project 2",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/python/python.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+
+		{
+			title: "Project 3",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/html/html.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+
+		{
+			title: "Project 4",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+
+		{
+			title: "Project 5",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+	],
+};
+
+export default INFO;
