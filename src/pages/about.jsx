50037a86b1e197d8c8dbede957b67b2d6d667269
Deven Nabar <dnabar@usc.edu>
2024-06-10 09:10:45 -0700

    Added tailwind footer and navbar

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -80,3 +80,84 @@ const About = () => {
 };
 
 export default About;
+// // TAILWIND START
+
+// import React, { useEffect } from "react";
+// import { Helmet } from "react-helmet";
+
+// import NavBar from "../components/common/navBar";
+// import Footer from "../components/common/footer";
+// import Logo from "../components/common/logo";
+// import Socials from "../components/about/socials";
+
+// import INFO from "../data/user";
+// import SEO from "../data/seo";
+
+// const About = () => {
+// 	useEffect(() => {
+// 		window.scrollTo(0, 0);
+// 	}, []);
+
+// 	const currentSEO = SEO.find((item) => item.page === "about");
+
+// 	return (
+// 		<>
+// 			<Helmet>
+// 				<title>{`About | ${INFO.main.title}`}</title>
+// 				<meta name="description" content={currentSEO.description} />
+// 				<meta
+// 					name="keywords"
+// 					content={currentSEO.keywords.join(", ")}
+// 				/>
+// 			</Helmet>
+
+// 			<div className="flex flex-col min-h-screen">
+// 				<NavBar active="about" />
+// 				<div className="flex flex-col flex-grow">
+// 					<div className="flex justify-start pt-6 pl-6">
+// 						<div className="fixed border border-white rounded-full shadow-md top-4">
+// 							<Logo width={46} />
+// 						</div>
+// 					</div>
+
+// 					<div className="flex flex-col flex-grow pt-24">
+// 						<div className="flex flex-col lg:flex-row">
+// 							<div className="flex flex-col lg:flex-row flex-grow">
+// 								<div className="lg:basis-4/5">
+// 									<h1 className="text-3xl font-bold">
+// 										{INFO.about.title}
+// 									</h1>
+// 									<p className="mt-4 text-lg">
+// 										{INFO.about.description}
+// 									</p>
+// 								</div>
+
+// 								<div className="lg:basis-1/4 flex flex-col">
+// 									<div className="rounded-full overflow-hidden shadow-xl transform rotate-0 mb-4">
+// 										<img
+// 											src="about.jpg"
+// 											alt="about"
+// 											className="block w-full"
+// 										/>
+// 									</div>
+// 									<div className="pt-12 pb-12">
+// 										<Socials />
+// 									</div>
+// 								</div>
+// 							</div>
+// 							<div className="lg:hidden pt-8">
+// 								<Socials />
+// 							</div>
+// 						</div>
+// 					</div>
+// 					<div className="mt-auto">
+// 						<Footer />
+// 					</div>
+// 				</div>
+// 			</div>
+// 		</>
+// 	);
+// };
+
+// export default About;
+// // TAILWIND END
